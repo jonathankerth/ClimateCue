2c4170182e24f290103f5c8e9829fbe3d1099bd3
jonathankerth <jonathanpkerth@gmail.com>
2023-11-20 19:27:36 -0800

    show add to favorite after signup

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { db } from "@/lib/firebase";
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import dynamic from "next/dynamic";
 import Weather from "../components/Weather";
 import TemperatureSwitch from "../components/TemperatureSwitch";
@@ -42,6 +42,22 @@ export default function Home() {
 			console.error("Error fetching favorite cities:", error);
 		}
 	};
+	const [currentUser, setCurrentUser] = useState(null);
+	useEffect(() => {
+		const auth = getAuth();
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in
+				setCurrentUser(user);
+			} else {
+				// No user is signed in
+				setCurrentUser(null);
+			}
+		});
+
+		// Cleanup subscription on unmount
+		return () => unsubscribe();
+	}, []);
 
 	useEffect(() => {
 		fetchFavoriteCities();
