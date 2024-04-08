b3e67fc42d20858d00561d0a8eb305377523852d
jonathankerth <jonathanpkerth@gmail.com>
2024-04-08 13:57:19 -0700

    justify items left

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,70 +10,66 @@ const Navbar = ({ isUserSubscribed }) => {
   }
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black/50 z-40">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              <div className="text-2xl font-bold text-neutral-100">
-                ClimateCue
-              </div>
-              {/* Current Weather Link */}
+    <nav className="fixed top-0 left-0 w-full bg-black/70 z-40">
+      <div className="max-w-7xl ml-4 px-4 sm:px-6">
+        <div className="flex items-center justify-start h-16">
+          <div className="text-2xl font-bold text-neutral-100">ClimateCue</div>
+          <div className="md:block  flex items-baseline space-x-4 ml-4">
+            {/* Current Weather Link */}
+            <Link
+              to="weather-data"
+              smooth={true}
+              duration={500}
+              className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+            >
+              Current Weather
+            </Link>
+
+            {/* Five Day Forecast */}
+            {!isUserSubscribed && (
               <Link
-                to="weather-data"
+                to="five-day-forecast"
                 smooth={true}
                 duration={500}
                 className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
               >
-                Current Weather
+                Five-Day Forecast
               </Link>
+            )}
 
-              {/* Five Day Forecast */}
-              {!isUserSubscribed && (
-                <Link
-                  to="five-day-forecast"
-                  smooth={true}
-                  duration={500}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Five-Day Forecast
-                </Link>
-              )}
-
-              {/* Outfit Recommendation */}
-              {isUserSubscribed && (
-                <Link
-                  to="outfit-rec"
-                  smooth={true}
-                  duration={500}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Outfit Recommendation
-                </Link>
-              )}
-
-              {/* Eight-Day Forecast Link - Conditionally Rendered */}
-              {isUserSubscribed && (
-                <Link
-                  to="eight-day-forecast"
-                  smooth={true}
-                  duration={500}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Eight-Day Forecast
-                </Link>
-              )}
-
-              {/* Weather Map Link */}
+            {/* Outfit Recommendation */}
+            {isUserSubscribed && (
               <Link
-                to="map"
+                to="outfit-rec"
                 smooth={true}
                 duration={500}
                 className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
               >
-                Weather Map
+                Outfit Recommendation
               </Link>
-            </div>
+            )}
+
+            {/* Eight-Day Forecast Link - Conditionally Rendered */}
+            {isUserSubscribed && (
+              <Link
+                to="eight-day-forecast"
+                smooth={true}
+                duration={500}
+                className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+              >
+                Eight-Day Forecast
+              </Link>
+            )}
+
+            {/* Weather Map Link */}
+            <Link
+              to="map"
+              smooth={true}
+              duration={500}
+              className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+            >
+              Weather Map
+            </Link>
           </div>
           <div className="-mr-2 flex md:hidden">
             {/* Mobile Menu Button */}
@@ -87,7 +83,7 @@ const Navbar = ({ isUserSubscribed }) => {
         </div>
         {/* Mobile Dropdown Menu */}
         {isOpen && (
-          <div className="md:hidden absolute top-16 w-full bg-neutral-800">
+          <div className="md:hidden absolute top-16 left-0 w-full bg-neutral-800">
             <div className="flex flex-col items-center space-y-2">
               {/* Current Weather Link */}
               <Link
