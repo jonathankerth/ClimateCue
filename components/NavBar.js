d302c8ee370ad85b897e20bdaa2ebf117ec9edbd
jonathankerth <jonathanpkerth@gmail.com>
2024-06-12 14:50:59 -0700

    change navbar to hamburger menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -15,78 +15,15 @@ const Navbar = ({ isUserSubscribed, firstName }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="hidden md:flex items-baseline space-x-4 ml-4">
-              <div className="text-2xl font-bold text-neutral-100">
-                ClimateCue
-              </div>
-              <p className="text-lg font-medium text-white">
-                Welcome,{" "}
-                <span className="text-white">{firstName || "Guest"}</span>
-              </p>
-              <Link
-                to="weather-data"
-                smooth={true}
-                duration={500}
-                offset={-64}
-                className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-              >
-                Current Weather
-              </Link>
-              <Link
-                onClick={toggleMenu}
-                to="weather-details"
-                smooth={true}
-                duration={500}
-                offset={-64}
-                className="text-neutral-100 hover:bg-primary hover:text-white px-4 py-2 rounded-md text-sm font-medium cursor-pointer"
-              >
-                Advanced Details
-              </Link>
-              {!isUserSubscribed && (
-                <Link
-                  to="five-day-forecast"
-                  smooth={true}
-                  duration={500}
-                  offset={-64}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Five-Day Forecast
-                </Link>
-              )}
-              {isUserSubscribed && (
-                <Link
-                  to="outfit-rec"
-                  smooth={true}
-                  duration={500}
-                  offset={-64}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Outfit Recommendation
-                </Link>
-              )}
-              {isUserSubscribed && (
-                <Link
-                  to="eight-day-forecast"
-                  smooth={true}
-                  duration={500}
-                  offset={-64}
-                  className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-                >
-                  Eight-Day Forecast
-                </Link>
-              )}
-              <Link
-                to="map"
-                smooth={true}
-                duration={500}
-                offset={-64}
-                className="text-neutral-100 hover:bg-primary hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
-              >
-                Weather Map
-              </Link>
+            <div className="text-2xl font-bold text-neutral-100">
+              ClimateCue
             </div>
+            <p className="text-lg font-medium text-white ml-4">
+              Welcome,{" "}
+              <span className="text-white">{firstName || "Guest"}</span>
+            </p>
           </div>
-          <div className="flex md:hidden">
+          <div className="flex">
             <button
               onClick={toggleMenu}
               className="inline-flex items-center justify-center p-2 rounded-md text-neutral-100 hover:text-white hover:bg-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -96,8 +33,8 @@ const Navbar = ({ isUserSubscribed, firstName }) => {
           </div>
         </div>
         {isOpen && (
-          <div className="md:hidden absolute top-16 left-0 w-full bg-neutral-800">
-            <div className="flex flex-col items-start space-y-2">
+          <div className="absolute top-16 left-0 w-full bg-neutral-800">
+            <div className="flex flex-col items-start space-y-2 p-4">
               <Link
                 onClick={toggleMenu}
                 to="weather-data"
